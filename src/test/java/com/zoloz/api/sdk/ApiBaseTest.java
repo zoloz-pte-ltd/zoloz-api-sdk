bdf9ac4de5b50145b474be08468dba33a64767be
jushi.zf <jushi.zf@antfin.com>
2019-12-25 17:52:59 +0800

    --amend

--- src/test/java/com/zoloz/api/sdk/ApiBaseTest.java
+++ src/test/java/com/zoloz/api/sdk/ApiBaseTest.java
@@ -41,10 +41,10 @@ public class ApiBaseTest {
 
     @Before
     public void init(){
-        String clientId = "2189400000000143";
-        String merchantPrivateKey = "MIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDcTm+BhyhynoFnHFcllxWp2LmFZqmK9mRfjrMs4Wfk0P0CwpW6h6CuJWZwHwd5iIL3ZT5yOM3yjE4J3035E3b+tGUJm64/NgfHC7u+9Teo8L/MJ+htmQDuq1fPBx0Mevhs7ObzI86olcMIM2eN3dFiI555evjGsXm9JXD5d1IGsgLX3lIgbj0BfFWxIMs1mCC5Pb4imExhDbDQQaWwLCZ0fdklO9o2jpB8He9MqCQWqF5PHe9s6Du1IWurTA6f6VCNWJwE+GiFnhVoMPOzJOSSeS9HvoKgRkQADJ1RlzrOKqTfJIcxOfkEoJI2pmkWMTGUVgyuuGt9UNQuqeLeVsVvAgMBAAECggEARMcH8LwtWHs7kuE6I7YjQT8zy/3c+FErzNlT/rLwKHNGSdJNxEoLU9LBREerobmIiYvGDwrE4PAEfPiF8ziscaAH2xQLNhi8HNwA5QDRO6DNgAMFgRG/RMOhSjTCUN8jJ35eK5IGNoNqu+iwb43MjILqOqq1d/J4OyCxQHCL3yKnkBx8k74B1qE+dtDnrQ9AMRgkkrV4JmWs1+f9n0/6zFfn2nnPXLh4O3umVW8lDxZjJ/tXsTgXSENuOCuSMdonNywODv9zBeKYI4n6M3n5sAQUKR0uoZQJff7r1xdbvMcSSp8TB8bGTNlaj1xsm/Lmc3texYa2q3wHAf8eyqcXQQKBgQDuRmGOhkbPQh7GMZhkiT2cJdlkNbx6ru/RJA3QQHCjEgBpgpcSp2T1IDOGCXyYb18DM30fdHzuDmsYNRY8bkdNCDZvDwezmOvzTJM9/pl/0coUu0p5QRv5zqUKPkvZ7zGJcyax2wB9gPahqhf1tuD8a9ykbhf7DKzqfHqbocfgqQKBgQDssd5M+8RcuRIficAspFv0IWBgZVnOcbsjBMdPA46K/PUAGLz67YG7JAM2cv+SSDNbOnog8UPO+RvYGKWAJV3VdWCSOK/En4zfHaQ+khvR7zUVFBGkyfx06MdLh8SpVw4jqKI5Jg4QahfauAL2/waBc+zXf/hSlVeoQb+PL0WMVwKBgQDJpgiT3LF6X4O5yWkl1DuutR1oQE3VKc1NGo0SLCbm6qosoNcHc2DuTiUsj0TBoKcxB1ch0qyTruIVZ3/8/nsbBxNCI98psAYnLiwCMEBpuvqhVuus+FwbqjVkGNqKgnJS13/szuF4VTbkFBYY3X9EQGhHQ+DJlq2T+Gqu4VhNmQKBgBDIf/HHfGHywYU4+EZyFQ8XesT7CsYnrFRMV3GT1/yk+R5borLMOoGxiQTIXLTbp8CVQemqEdvQMBg/jySqAaBMJV/Y4yW3Bn8fk/DX3V8ibgojlSixJdSIWPFTHC/Bu3/tyirxNtgVHcHlgFhRNv3g4W9aZJo+equ0lceqW20bAoGBAOG6ySw9zn1BsHGSv40+w2fO4YmTv/P5GazVmrf3UWoTz6g5r6ts8NOtka8m4cZxOm6DJu3AgyE7yRx/5cwI8+IFFrp/mK5iXodWE+DdR1lQDVvq+4WgtlretrHF8W9zIRLG2zSakZ9sXQQycqXF+YBly6n/zDwE905754RH4P60";
-        String openApiPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhFkEaT2orLpL+lbDdDp1I7b9iG7E+D7DqZWsmy84e7Tm33kUOXYjOuZhnASevVowDyDbumJq4++d1FnA/jUy+Dmi8hkL10qztDaLod4opfzFXAfq3p3PsjOr0qN7wvX/nu+Ko2A0xO/2elnih3iIXMxU2K988+TspQLQZXN+ZyaJTF4uB1BgczQgDTHaC7eVPbS0AZWs61/b3PfhIqvT/DkYUva3yHciAthQTOVv6P4Yxjoid1huthFYob6MwsCrD62m4GamYACsQTSZgmvU49XbcGxnnvnHmwb1B/KHqA78U8YXL2MHoZZODLzd51JUUmdVZAv1Cr6J2NHiRbN3/QIDAQAB";
-        String hostUrl = "https://open-sea.zoloz.com";
+        String clientId = "";
+        String merchantPrivateKey = "";
+        String openApiPublicKey = "";
+        String hostUrl = "";
 
         // initialize OpenApiClient
         client= new OpenApiClient();  // construct with signature and encryption by default
