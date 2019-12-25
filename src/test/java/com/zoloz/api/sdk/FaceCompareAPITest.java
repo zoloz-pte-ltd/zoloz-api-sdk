9c6bb3a5638fb63f441d5885145c257b48b85051
jushi.zf <jushi.zf@antfin.com>
2019-12-25 16:47:00 +0800

    fix face test

--- src/test/java/com/zoloz/api/sdk/FaceCompareAPITest.java
+++ src/test/java/com/zoloz/api/sdk/FaceCompareAPITest.java
@@ -26,6 +26,7 @@ import com.alibaba.fastjson.JSON;
 import com.zoloz.api.sdk.api.FaceCompareAPI;
 import com.zoloz.api.sdk.model.FaceCompareRequest;
 import com.zoloz.api.sdk.model.FaceCompareResponse;
+import org.junit.Assert;
 import org.junit.Test;
 
 /**
@@ -40,7 +41,26 @@ public class FaceCompareAPITest extends ApiBaseTest {
     private String face2Url = "/images/face/face2.jpg";
 
     @Test
-    public void testFaceCompare() {
+    public void testSameFace() {
+        // initialize FaceCompareApi
+        FaceCompareAPI faceCompareApi = new FaceCompareAPI(getClient());
+
+        // prepare request body
+        FaceCompareRequest request = new FaceCompareRequest();
+        request.setBizId("test-biz-id");
+        request.getFace1().setContent(getBase64ImageContent(face1Url));
+        request.getFace1().setRect("112,234,455,89");
+        request.getFace2().setContent(getBase64ImageContent(face1Url));
+        request.getFace2().setRect("112,234,455,89");
+        FaceCompareResponse response = faceCompareApi.compare(request);
+        System.out.println(JSON.toJSONString(response));
+        Assert.assertEquals("S", response.getResult().getResultStatus());
+        Assert.assertTrue(response.getSamePerson());
+    }
+
+
+    @Test
+    public void testDifferentFace() {
         // initialize FaceCompareApi
         FaceCompareAPI faceCompareApi = new FaceCompareAPI(getClient());
 
@@ -53,6 +73,8 @@ public class FaceCompareAPITest extends ApiBaseTest {
         request.getFace2().setRect("543,544,112,56");
         FaceCompareResponse response = faceCompareApi.compare(request);
         System.out.println(JSON.toJSONString(response));
+        Assert.assertEquals("S", response.getResult().getResultStatus());
+        Assert.assertFalse(response.getSamePerson());
     }
 
 }
